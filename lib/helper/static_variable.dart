3b481ae1bdf206790d014ddd9953be32505827b5
bagusyanuar <bagus.yanuar613@gmail.com>
2023-01-23 12:23:22 +0700

    oke

--- lib/helper/static_variable.dart
+++ lib/helper/static_variable.dart
@@ -1,2 +1,2 @@
-const HostAddress = "http://192.168.137.1:8002/api";
-const BaseHostImage = "http://192.168.137.1:8002";
+const HostAddress = "http://192.168.43.119:8002/api";
+const BaseHostImage = "http://192.168.43.119:8002";
